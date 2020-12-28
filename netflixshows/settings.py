fe7f5b94c5255e1d37d2e1ad2b55bebb88278cb3
mehedihassan <mehedi_45seafarer@yahoo.com>
2020-12-28 06:08:10 -0500

    Settings modified for aws deploy

--- netflixshows/settings.py
+++ netflixshows/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*&gal3+zlvo!$wdf$mgs05$c)=e@wrm790)d$&*bz6776$3v@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
